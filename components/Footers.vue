e60f2377ae84632168c853acd912909406e9f6ae
liuyang <liuyang@metadata.net.cn>
2020-03-04 10:41:44 +0800

    fix: 添加链接备案

--- components/Footers.vue
+++ components/Footers.vue
@@ -2,7 +2,9 @@
   <footer>
     <div class="wrapper">
       <div class="copyright"><a href="/">www.feblog.cn</a> - 专注前端博客 Copyright© Author thisliuyang@gmail.com</div>
-      <div>鄂ICP备19004686号-3</div>
+      <div>
+        <a href="http://www.beian.miit.gov.cn">鄂ICP备19004686号-3</a>
+      </div>
     </div>
   </footer>
 </template>
